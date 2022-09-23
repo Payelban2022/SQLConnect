1fbc2f3c5764948d9f31d795b16be1e703c0e232
Payel Banerjee <payel.nandy2580@gmail.com>
2022-09-23 12:58:24 -0400

    sql connect

---
+++ paysystem.py
@@ -0,0 +1,75 @@
+import mysql.connector
+mydb = mysql.connector.connect(
+    host= 'localhost',
+    username = 'payel',
+    password = 'payel1987',
+    database = "Employee"
+)
+print(mydb)
+
+mycursor = mydb.cursor()
+# mycursor.execute("Create database Employee")
+
+mycursor.execute("Show databases")
+for x in mycursor:
+    print (x)
+# mycursor.execute("create table EmployeeIn (EmpID int primary key, Name varchar(100), Department varchar(100))")
+# mycursor.execute("create table Salary(EmpID int, Pay  int)")
+mycursor.execute("Show tables")
+for x in mycursor:
+    print (x)
+
+# sql = "Insert into EmployeeIn(EmpID, Name, Department) values (%s,%s,%s)"
+# val = [
+#   ('1','Peter', 'Admn'),
+#   ('2','Amy', 'Software'),
+#   ('3','Hannah', 'Data Analyst'),
+#   ('4','Michael', 'Payroll'),
+#   ('5','Sandy', 'Data Engineer'),
+#   ('6','Betty', 'House Keeper'),
+#   ('7','Richard', 'Java Developer'),
+#   ('8','Susan', 'Manager'),
+#   ('9','Vicky', 'Customer Care'),
+#   ('10','Ben', 'Receptionist')
+#
+# ]
+
+
+
+# mycursor.executemany(sql, val)
+#
+# mydb.commit()
+
+mycursor.execute("Select * from EmployeeIn")
+myresult = mycursor.fetchall()
+
+
+
+for x in myresult:
+    print (x)
+# mycursor.execute("Alter table Salary add column RevisedDate DATE")
+# sql = "Insert into Salary (EmpID,Pay, RevisedDate) values (%s,%s, %s)"
+# val = [
+#   ('1', '500', '2022-03-01' ),
+#   ('2','1000', '2022-09-01'),
+#   ('3','700','2022-09-06'),
+#   ('4','600','2022-03-01'),
+#   ('5','750','2022-07-01'),
+#   ('6','400','2022-03-01'),
+#   ('7','900','2022-06-01'),
+#   ('8','1200','2022-04-01'),
+#   ('9','450','2022-03-01'),
+#   ('10','400','2022-03-01')
+#
+#  ]
+
+# mycursor.executemany(sql, val)
+#
+# mydb.commit()
+
+mycursor.execute("Select * from Salary")
+myresult = mycursor.fetchall()
+for x in myresult:
+    print (x)
+
+
