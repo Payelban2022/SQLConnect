998dcab0fed3ad88565739842602978b82d80652
Payel Banerjee <payel.nandy2580@gmail.com>
2022-09-22 22:31:16 -0400

    sql connect

--- new.py
+++ new.py
@@ -11,11 +11,11 @@ print(mydb)
 
 import mysql.connector
 
-mydb = mysql.connector.connect(
-  host="localhost",
-  user="payel",
-  password="payel1987"
-)
+# mydb = mysql.connector.connect(
+#   host="localhost",
+#   user="payel",
+#   password="payel1987"
+# )
 
 # mycursor = mydb.cursor()
 #
@@ -23,11 +23,11 @@ mydb = mysql.connector.connect(
 
 import mysql.connector
 
-mydb = mysql.connector.connect(
-  host="localhost",
-  user="payel",
-  password="payel1987"
-)
+# mydb = mysql.connector.connect(
+#   host="localhost",
+#   user="payel",
+#   password="payel1987"
+# )
 
 mycursor = mydb.cursor()
 
@@ -47,14 +47,14 @@ for x in mycursor:
 # mycursor.execute("select * from PaySystem.employee")
 # mycursor.execute("insert into employee (name, department, pay) values ('Payel Banerjee', 'Bill', '500')")
 
-sql = "INSERT INTO PaySystem.employee (name, department, pay) VALUES (%s, %s, %s)"
-val = ("John", "Bill", "500")
-
-
-for i in range(600,700):
-  val = ("John", "Bill", str(i))
-  mycursor.execute(sql, val)
-mydb.commit()
+# sql = "INSERT INTO PaySystem.employee (name, department, pay) VALUES (%s, %s, %s)"
+# val = ("John", "Bill", "500")
+#
+#
+# for i in range(600,700):
+#   val = ("John", "Bill", str(i))
+#   mycursor.execute(sql, val)
+# mydb.commit()
 
 mycursor.execute("select * from PaySystem.employee")
 for x in mycursor:
@@ -70,3 +70,56 @@ for x in mycursor:
 mycursor.execute("select name, pay from employee where pay = (select max(pay) from employee)")
 for x in mycursor:
   print(x)
+
+# mycursor.execute("Create database EmployeeSystem")
+# mycursor.execute("Create table employeedata  (id int primary key auto_increment, name varchar(50), year_join  date)")
+mycursor.execute("Show tables")
+for x in mycursor:
+  print(x)
+
+sql = "INSERT INTO employeedata(name, year_join)  values(%s,%s)"
+val = [('John','2020-01-12'),('Peter', '2020-01-01')]
+mycursor.executemany(sql,val)
+mydb.commit
+
+mycursor.execute("Select * from employeedata")
+
+myresult = mycursor.fetchall()
+for x in myresult:
+  print(x)
+
+sql = ( "Select * from employeedata where name = 'John' ")
+mycursor.execute(sql)
+myresult = mycursor.fetchall()
+
+for x in myresult:
+  print(x)
+
+sql = ("Select * from employee order by pay desc limit 5 offset 2")
+mycursor.execute(sql)
+myresult = mycursor.fetchall()
+for x in myresult:
+  print(x)
+
+sql = ("update employee set department = 'Admn' where department = 'Bill'")
+mycursor.execute(sql)
+mydb.commit()
+sql = ("select * from employee limit 5 ")
+mycursor.execute(sql)
+myresult = mycursor.fetchall()
+for x in myresult:
+  print(x)
+
+# sql = "select " \
+#       "employee.name," \
+#       " employeedata.year_join, " \
+#       "employee.pay, "
+# 'employee.department' \
+# 'from employee ' \
+# 'join employeedata ' \
+# 'on employee.name = employeedata.name'
+# mycursor.execute(sql)
+# myresult = mycursor.fetchall()
+# for x in myresult:
+#   print(x)
+
